0ce6a64e409c880e1f0138c4ebcb2b9a5f78feaa
Kirk Rohani <kirk.rohani@gmail.com>
2017-04-23 20:38:09 -0500

    created index.js with express and initial hello.html file

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.set('port', process.env.PORT || 3000);
+
+http.createServer(app)
+  .listen( app.get('port'), console.log(`Express Server listening on port ${app.get('port')}...`) );
