72118fdaa9279f33cd649e7565f3c7bd47b4995c
Kirk Rohani <kirk.rohani@gmail.com>
2017-04-25 15:06:25 -0500

    added rendering formula bar when cell clicked and updating model when data enterted into formular bar for a particular cell

--- client/js/TableView.js
+++ client/js/TableView.js
@@ -1,4 +1,4 @@
-const { removeChildren, createTH } = require("./DOMUtils.js");
+const { removeChildren, createTH, createTR, createTD } = require("./DOMUtils.js");
 const { getLetterRange } = require("./ArrayUtils.js");
 
 class TableView {
@@ -9,25 +9,91 @@ class TableView {
 
   init() {
     this.initDOMReference();
+    this.initCurrentCellLocation();
     this.renderTable();
+    this.renderFormulaBar();
+    this.attachEventHandlers();
+    //TODO REMOVE console.log(`FORMULA BAR ->${this.formulaBar.value}<-`);
+    
   }
 
   initDOMReference() {
-    this.tableHeaderRow = document.querySelector("THEAD TR");
+    this.tableHeader = document.querySelector("THEAD TR");
+    this.tableBody = document.querySelector("TBODY");
+    this.formulaBar = document.querySelector("#formula");
     
+  }
+
+  initCurrentCellLocation() {
+    this.currentCellLocation = { "col": 0, "row": 0 };
+  }
+
+  renderFormulaBar() {
+    const cellLocationForDisplay = `row ${this.currentCellLocation.row} : col ${this.currentCellLocation.col} `;
+    this.formulaBar.placeholder = cellLocationForDisplay;
+
+    this.formulaBar.value = this.model.getValue(this.currentCellLocation) || "";
   }
 
   renderTable() {
     this.renderTableHeader();
+    this.renderTableBody();
   }
 
   renderTableHeader() {
-    removeChildren(this.tableHeaderRow);
+    removeChildren(this.tableHeader);
     getLetterRange('A', this.model.cols)
       .map( letter => createTH(letter) )
-      .forEach( TH => this.tableHeaderRow.appendChild(TH) );
+      .forEach( TH => this.tableHeader.appendChild(TH) );
   }
 
+  isCurrentCell(row, col) {
+    return this.currentCellLocation.row === row && this.currentCellLocation.col === col;
+  }
+
+  renderTableBody() {
+    removeChildren(this.tableBody);
+    const fragment = document.createDocumentFragment();
+
+    for (let row=0; row < this.model.rows; row++) {
+      const tableRow = createTR();
+      
+      for (let col=0; col < this.model.cols; col++) {
+        const location = {"col": col, "row": row};
+        //TODO REMOVE console.log(`Rending row ${row} col ${col} with value ${this.model.getValue(location)}`);
+        const tableCell = createTD(this.model.getValue(location));
+
+        if(this.isCurrentCell(row, col))
+          tableCell.className = "currentCell";
+        tableRow.appendChild(tableCell);
+      }
+      fragment.appendChild(tableRow);
+    }
+    this.tableBody.appendChild(fragment);
+
+    
+  }
+
+  handleCellClick(event) {
+    const col = event.target.cellIndex;
+    const row = event.target.parentElement.rowIndex -1;
+
+    this.currentCellLocation = {"col": col, "row": row};
+    this.renderTableBody();
+    this.renderFormulaBar();
+  }
+
+  handleFormulaBarUserInput() {
+    this.formulaBar.placeholder = "";
+    const userInput = this.formulaBar.value;
+    this.model.setValue(this.currentCellLocation, userInput);
+    this.renderTableBody();
+  }
+
+  attachEventHandlers() {
+    this.tableBody.addEventListener("click", this.handleCellClick.bind(this) );
+    this.formulaBar.addEventListener("keyup", this.handleFormulaBarUserInput.bind(this) );
+  }
 }
 
 module.exports = TableView;
---
+++ client/js/test/TableView.test.js
@@ -0,0 +1,101 @@
+const TableView = require("../TableView.js");
+const TableModel = require("../TableModel.js");
+const fs = require("fs");
+
+describe("CLASS TableView Test Suite", () => {
+
+  beforeEach(() => {
+    const fixturePath = "./client/js/test/fixtures/sheet-container.html";
+    const html = fs.readFileSync(fixturePath, "utf8");
+    document.documentElement.innerHTML = html;
+  });
+
+  describe("Test Table Header", () => {
+    it("has valid header row labels", () => {
+      const model = new TableModel(10, 4);
+      const view = new TableView(model);
+      view.init();
+
+      let tableHeaderRow = document.querySelector("THEAD TR");
+      expect(tableHeaderRow.childNodes.length).toEqual(model.cols);
+
+      let cellValues = Array.from(tableHeaderRow.childNodes)
+        .map( node => node.textContent );
+      expect(cellValues).toEqual(['A', 'B', 'C', 'D']);
+    });
+  });
+
+  describe("Test Table Body", () => {
+    it("has the right number of rows and columns", () => {
+      const model = new TableModel(10, 4);
+      const view = new TableView(model);
+      view.init();
+
+      let tableBody = document.querySelector("TBODY");
+      expect(tableBody.childNodes.length).toEqual(model.rows);
+      expect(tableBody.childNodes[0].childNodes.length).toEqual(model.cols);
+    });
+
+    it("has the same data as in the model", () => {
+      const model = new TableModel(10, 4);
+      const view = new TableView(model);
+      const location = {"col":0, "row":0};
+      model.setValue(location, 101);
+      view.init();
+
+      let tableBody = document.querySelector("TBODY");
+      
+      expect(tableBody.childNodes[0].childNodes[0].textContent).toEqual('101');
+    });
+
+    it("highlights the cell when clicked", () => {
+      const model = new TableModel(5, 5);
+      const view = new TableView(model);
+      view.init();
+
+      const tableBody = document.querySelector("TBODY");
+      let tableCell = tableBody.childNodes[1].childNodes[1];
+      expect(tableCell.className).toBe("");
+
+      tableCell.click();
+      tableCell = tableBody.childNodes[1].childNodes[1];
+      expect(tableCell.className).not.toBe("");
+    });
+  });
+
+  describe("Test Formula Bar", () => {
+    it("updates the formula bar when user clicks a cell with data", () => {
+      const model = new TableModel(3, 3);
+      const view = new TableView(model);
+      view.init();
+
+      const formulaBar = document.querySelector("#formula");
+      expect(formulaBar.value).toBe("");
+
+      
+      model.setValue({"col": 1, "row": 1}, 35);
+      const tableCell = document.querySelector("TBODY").childNodes[1].childNodes[1];
+      tableCell.click();
+      expect(formulaBar.value).toBe("35");
+    });
+
+
+    it("updates the cell when input entered into formula bar", () => {
+      const model = new TableModel(3, 3);
+      const view = new TableView(model);
+      view.init();
+
+      const formulaBar = document.querySelector("#formula");
+      expect(formulaBar.value).toBe("");
+
+      const tableCell = document.querySelector("TBODY").childNodes[1].childNodes[1];
+      tableCell.click();
+      
+      formulaBar.value = "kirk";
+      view.handleFormulaBarUserInput();
+
+      expect(model.getValue({"col": 1, "row": 1})).toBe("kirk");
+    });
+  });
+
+});
